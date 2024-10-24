e7650032af5b25eef1ee0b1faa487eece0925983
김민규 <tjfvndfud@gmail.com>
2024-10-24 14:10:03 +0900

    add: 모달 테스트용 페이지 추가

---
+++ src/app/(test)/modal-test/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Modal from "@/_components/modal/Modal";
+import ModalButton from "@/_components/modal/ModalButton";
+import { useState } from "react";
+
+const ModalTestPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalOpen = () => setIsOpen(true);
+  const handleModalClose = () => setIsOpen(false);
+
+  return (
+    <div>
+      <h1>모달 테스트</h1>
+      <ModalButton
+        buttonText="모달 열기"
+        onClick={handleModalOpen} // 버튼 클릭 시 모달 열기
+      />
+
+      <Modal modalClass={""} isOpen={isOpen} onClose={handleModalClose}>
+        <h2>모달 내용</h2>
+        <p>모달 내의 텍스트입니다.</p>
+        <button onClick={handleModalClose}>닫기</button>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalTestPage;
