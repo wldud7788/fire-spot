e28e6409e5b23efea8c0cc830c63ffa47bf3701c
김민규 <tjfvndfud@gmail.com>
2024-10-27 18:53:26 +0900

    add: 모임 카드 컴포넌트 적용 1. src/_components/meet/ : 모임 카드 컴포넌트 2.  src/app/(pages)/meets/types/ : 모임 카드 타입 정의 (더미 데이터 기반 추후 변경 필요) 3. src/app/(test)/ : 카드 ui 확인용 테스트 페이지

---
+++ src/_components/meet/MeetCard.tsx
@@ -0,0 +1,49 @@
+import { Meet } from "@/app/(pages)/meets/types/Meet";
+import Link from "next/link";
+
+type MeetCardProps = {
+  meet: Meet;
+};
+
+const MeetCard = ({ meet }: MeetCardProps) => {
+  return (
+    <div className="communityCard w-[calc(50%-50px)] rounded-[20px] bg-[#F4F4F4] px-[30px] py-[25px]">
+      <div className="inner">
+        <div className="utils flex items-center justify-between">
+          <ul className="flex gap-[10px]">
+            {meet.tags.map((tag) => {
+              return (
+                <li
+                  key={tag}
+                  className="rounded-[15px] bg-[#D9D9D9] px-[15px] py-[3px] text-[15px]"
+                >
+                  {tag}
+                </li>
+              );
+            })}
+          </ul>
+          <button type="button">북마크</button>
+        </div>
+        <Link
+          href="#"
+          className="mb-[10px] mt-[10px] block text-[30px] font-medium"
+        >
+          {meet.title}
+        </Link>
+        <div className="info flex gap-[20px]">
+          <p className="bg-location bg-left-center bg-no-repeat pl-[20px] text-[16px]">
+            {meet.location}
+          </p>
+          <p className="bg-date bg-left-center bg-no-repeat pl-[18px] text-[16px]">
+            {meet.date}
+          </p>
+          <p className="bg-group bg-left-center bg-no-repeat pl-[18px] text-[16px]">
+            {meet.personnelCount01}/{meet.personnelCount02}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MeetCard;
---
+++ src/app/(test)/meet-test/page.tsx
@@ -0,0 +1,42 @@
+import MeetCard from "@/_components/meet/MeetCard";
+
+const MeetsTestPage = () => {
+  const meets = [
+    {
+      id: 0,
+      tags: ["마감임박", "글램핑"],
+      title: "불멍좋아하는사람 모여라!",
+      location: "영양군",
+      date: "10.25(수)",
+      personnelCount01: 5,
+      personnelCount02: 10
+    },
+    {
+      id: 1,
+      tags: ["마감임박22", "글램핑22"],
+      title: "불멍좋아하는사람 모여라!22",
+      location: "영양군",
+      date: "10.25(수)",
+      personnelCount01: 5,
+      personnelCount02: 10
+    },
+    {
+      id: 2,
+      tags: ["마감임박33", "글램핑33"],
+      title: "불멍좋아하는사람 모여라!33",
+      location: "영양군",
+      date: "10.25(수)",
+      personnelCount01: 5,
+      personnelCount02: 10
+    }
+  ];
+  return (
+    <div className="flex flex-wrap items-center gap-[50px]">
+      {meets.map((meet, idx) => {
+        return <MeetCard key={idx} meet={meet} />;
+      })}
+    </div>
+  );
+};
+
+export default MeetsTestPage;
