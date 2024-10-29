a938615fcad881fc6b16d60479c6894d5fbb2b8c
김민규 <tjfvndfud@gmail.com>
2024-10-30 01:10:26 +0900

    add: 후기 카드 default type 1차 적용 - 데이터가 없어서 더미 데이터로 적용 - props의 타입 설정하여 main, default로 조건 처리

---
+++ src/_components/feed/FeedCard.tsx
@@ -0,0 +1,46 @@
+import { FeedItem } from "@/app/(pages)/feeds/types/Feed";
+
+type FeedCardProps = {
+  feed: FeedItem;
+  type?: string;
+};
+
+const FeedCard = ({ feed, type }: FeedCardProps) => {
+  return (
+    <>
+      {type ? (
+        // type main
+        <div className="feed_card type_main">
+          메인 후기 카드 타입으로 적용 필요
+        </div>
+      ) : (
+        // type default
+        <div className="feed_card">
+          <div className="inner">
+            <div className="user_info">
+              <div>{feed.profileImg}</div>
+              <div>{feed.userName}</div>
+              <div>{feed.time}</div>
+            </div>
+            <div className="feed_desc">
+              <p>{feed.desc}</p>
+            </div>
+            <div className="utils">
+              <div className="btn_area">
+                {/* 좋아요 컴포넌트 적용 필요*/}
+                <button>좋아요 1개</button>
+                <button>답글 달기</button>
+              </div>
+              <div className="img_box">
+                {/* 이미지 겹쳐있는 컴포넌트 확인 필요*/}
+                이미지박스
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FeedCard;
