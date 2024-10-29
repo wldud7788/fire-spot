b185b01e183f8323c4c80c008ebec1538d1108ad
김민규 <tjfvndfud@gmail.com>
2024-10-29 23:36:23 +0900

    add: 메인 1차 작업 완료 - MainSection: 공통 섹션 컴포넌트 - MainTop: 메인 최상단 영역, 검색바 추가 필요 - MainSlide: 메인 슬라이드 영역, Slide 컴포넌트로 구성됨 - MainUserCard: 메인 유저 정보 영역, 비회원/로그인 조건 처리 - MainIcons: 메인 아이콘 리스트 영역, 이미지와 텍스트 전달받으면 처리 - MainCamps: 메인 캠프 리스트 영역, getTotalData 함수를 통해 리스트 출력 [대기중] - 메인 최상단 검색바 적용 필요 - 아이콘 리스트 적용 필요 - 후기 리스트 적용 필요

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import MainButton from "@/_components/main/MainButton";
+import MainCamps from "@/_components/main/MainCamps";
 import MainIcons from "@/_components/main/MainIcons";
 import MainSection from "@/_components/main/MainSection";
+import MainSlide from "@/_components/main/MainSlide";
 import MainTop from "@/_components/main/MainTop";
+import MainUserCard from "@/_components/main/MainUserCard";
+
 import {
   dehydrate,
   HydrationBoundary,
@@ -10,6 +13,7 @@ import {
 
 export default function Home() {
   const queryClient = new QueryClient();
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <div className="main_area">
@@ -22,20 +26,12 @@ export default function Home() {
 
           {/* main_sec01 */}
           <MainSection background="#F2F2F2">
-            <div className="flex items-center gap-[20px]">
+            <div className="flex items-center justify-stretch gap-[20px]">
               <div className="w-[calc(100%-465px)] rounded-[20px]">
-                슬라이더 위치
+                <MainSlide />
               </div>
               <div className="flex-1 rounded-[20px] bg-[#E0E0E0] p-[40px]">
-                <div className="mb-[20px] flex items-center">
-                  <img src="/assets/images/ico-fire-spot.svg" />
-                  <p className="text-[24px] font-bold">이준열 님</p>
-                </div>
-                <div className="mb-[15px] flex gap-[15px]">
-                  <MainButton text={"내 모임"} />
-                  <MainButton text={"내 후기"} />
-                </div>
-                <MainButton text={"캠핑 꿀팁 바로가기"} />
+                <MainUserCard />
               </div>
             </div>
           </MainSection>
@@ -52,6 +48,7 @@ export default function Home() {
             <h2 className="flex items-center justify-center text-center text-[32px] font-bold">
               캠핑장
             </h2>
+            <MainCamps />
           </MainSection>
           {/*// main_sec02 */}
 
