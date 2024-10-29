9f73b0ab8fe4defdc4da9e0ab8dd6e0e0d22ec41
김민규 <tjfvndfud@gmail.com>
2024-10-29 23:21:17 +0900

    fix: 메인 버튼 컴포넌트 props로 href값 없어서 적용해줬습니다.

--- src/_components/main/MainButton.tsx
+++ src/_components/main/MainButton.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 interface MainButtonProps {
   text: string;
+  href: string;
 }
 
-const MainButton = ({ text }: MainButtonProps) => {
+const MainButton = ({ text, href }: MainButtonProps) => {
   return (
     <Link
-      href={"#"}
+      href={href}
       className="flex h-[60px] flex-1 items-center justify-center rounded-[15px] bg-[#f2f2f2] text-[20px] font-extrabold text-[#404040]"
     >
       {text}
