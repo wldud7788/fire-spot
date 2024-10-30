b886ca4a529385c9df7b3c99606aace64f0d6866
김민규 <tjfvndfud@gmail.com>
2024-10-31 05:01:12 +0900

    update: 기존 리스트페이지, 상세페이지에서 리스트 페이지 url을 적용하기 위해 수정함

--- src/app/(pages)/camps/[campPageId]/page.tsx
+++ src/app/(pages)/camps/[campPageId]/page.tsx
@@ -1,12 +1,20 @@
 import CampList from "@/_components/camp/CampList";
 import { getTotalData } from "@/_utils/serverActions/campApi";
 
-const CampListPage = async () => {
+type CampListProps = {
+  params: {
+    campPageId: string;
+  };
+};
+
+const CampListPage = async ({ params }: CampListProps) => {
   const camps = await getTotalData();
 
   if (!camps) return <div>데이터가 없음</div>;
 
-  return <CampList camps={camps} itemsPerPage={8} />;
+  return (
+    <CampList camps={camps} itemsPerPage={8} paramsId={params.campPageId} />
+  );
 };
 
 export default CampListPage;
