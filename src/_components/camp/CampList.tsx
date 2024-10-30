3a8e08d90be0e8bea598147b937d03cb1061f228
김민규 <tjfvndfud@gmail.com>
2024-10-31 04:58:58 +0900

    update: 기존 리스트에서 상태처리 및 함수 사용한 부분 custom hook 으로 변경

--- src/_components/camp/CampList.tsx
+++ src/_components/camp/CampList.tsx
@@ -2,31 +2,18 @@
 
 import CampCard from "@/_components/camp/CampCard";
 import { Camp } from "@/app/(pages)/camps/types/Camp";
-import { useState } from "react";
 import Pagination from "../pagination/Pagination";
+import usePagination from "../pagination/pagination-hook";
 
 type CampListProps = {
   camps: Camp[];
   itemsPerPage: number;
+  paramsId: string;
 };
 
-const CampList = ({ camps, itemsPerPage }: CampListProps) => {
-  const [page, setPage] = useState<number>(1);
-
-  const firstItems = (page - 1) * itemsPerPage;
-  const lastItems = page * itemsPerPage;
-  const currentItems = camps.slice(firstItems, lastItems);
-  const totalPages = Math.round(camps.length / itemsPerPage);
-
-  const handleMovePagePrev = () => {
-    if (page <= 1) return false;
-    setPage((prev) => prev - 1);
-  };
-
-  const handleMovePageNext = () => {
-    if (page == totalPages) return false;
-    setPage((prev) => prev + 1);
-  };
+const CampList = ({ camps, itemsPerPage, paramsId }: CampListProps) => {
+  const { currentItems, page, totalPages, movePagePrev, movePageNext } =
+    usePagination({ items: camps, itemsPerPage, paramsId });
 
   return (
     <div className="camp_list">
@@ -40,8 +27,8 @@ const CampList = ({ camps, itemsPerPage }: CampListProps) => {
       <Pagination
         page={page}
         totalPages={totalPages}
-        onMovePagePrev={handleMovePagePrev}
-        onMovePageNext={handleMovePageNext}
+        onMovePagePrev={movePagePrev}
+        onMovePageNext={movePageNext}
       />
     </div>
   );
