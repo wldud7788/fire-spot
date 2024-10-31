56c1f14ab993de383cbb141e69eacc5dad8448c9
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-31 17:40:25 +0900

    add: 모임 상세 CSS 완료 (지도, 날씨, 채팅 제외, 반응형 제외, 폰트 제외)

--- src/app/(pages)/meets/components/meetsDetail/MeetSuppliesSection.tsx
+++ src/app/(pages)/meets/components/meetsDetail/MeetSuppliesSection.tsx
@@ -6,11 +6,16 @@ type Props = {
 const MeetSuppliesSection = ({ meetWithCamp }: Props) => {
   const { meet } = meetWithCamp;
   return (
-    <div className="mt-10">
-      <h3>준비물</h3>
-      <ul className="flex gap-4">
+    <div className="mt-12 flex w-full flex-col gap-7">
+      <h2 className="text-4xl">준비물</h2>
+      <ul className="flex gap-6">
         {meet.supplies.map((item, idx) => (
-          <li key={idx}>{item}</li>
+          <li
+            key={idx}
+            className="rounded-3xl border-[2px] border-black pb-2 pl-7 pr-7 pt-2"
+          >
+            <span className="text-xl">{item}</span>
+          </li>
         ))}
       </ul>
     </div>
