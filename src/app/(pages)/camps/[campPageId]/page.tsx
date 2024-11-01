764c1fc92fd065a920951f6b0b8044bd9064ef1d
김민규 <tjfvndfud@gmail.com>
2024-11-01 17:35:26 +0900

    update: 서버컴포넌트 -> 클라이언트 컴포넌트로 변경 후 react query로 데이터 패칭 적용

--- src/app/(pages)/camps/[campPageId]/page.tsx
+++ src/app/(pages)/camps/[campPageId]/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import CampList from "@/_components/camp/CampList";
 import { getTotalData } from "@/_utils/serverActions/campApi";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 type CampListProps = {
   params: {
@@ -7,10 +11,27 @@ type CampListProps = {
   };
 };
 
-const CampListPage = async ({ params }: CampListProps) => {
-  const camps = await getTotalData();
+const CampListPage = ({ params }: CampListProps) => {
+  const {
+    data: camps,
+    isLoading,
+    isError,
+    refetch
+  } = useQuery({
+    queryKey: ["camps"],
+    queryFn: async () => getTotalData(),
+    refetchOnMount: true // 마운트 시 재요청
+  });
 
-  if (!camps) return <div>데이터가 없음</div>;
+  console.log(params);
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  if (isLoading) return <div>데이터가 로딩중입니다.</div>;
+  if (isError) return <div>에러가 발생했습니다.</div>;
+  if (!camps || camps.length === 0) return <div>데이터가 없습니다.</div>;
 
   return (
     <CampList camps={camps} itemsPerPage={8} paramsId={params.campPageId} />
