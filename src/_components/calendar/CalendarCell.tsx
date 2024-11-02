bd8f497678446feeb9860374d0c88bbc6259b328
LeeJY97 <ijoonyeol97@gmail.com>
2024-11-03 01:42:58 +0900

    add: 일정 mouseOver 이벤트, onClick 이벤트 추가

--- src/_components/calendar/CalendarCell.tsx
+++ src/_components/calendar/CalendarCell.tsx
@@ -2,15 +2,24 @@ import { format } from "date-fns";
 import React from "react";
 import { CellCard } from "./type/schedule.types";
 import { getMeetCardStyle } from "./utils/calenderService";
+import { useRouter } from "next/navigation";
 
 type Props = {
   day: Date;
   cellCardList: CellCard[];
+  hoverItem: string;
+  handleHoverItem: (id: string) => void;
 };
 
 // TODO 스탬프, 모임 필터링 데이터는 여기서 하는게 나을까?
 // 캘린더 한 칸
-const CalendarCell = ({ day, cellCardList }: Props) => {
+const CalendarCell = ({
+  day,
+  cellCardList,
+  hoverItem,
+  handleHoverItem
+}: Props) => {
+  const router = useRouter();
   const showStamp = true;
 
   if (!cellCardList) {
@@ -36,10 +45,22 @@ const CalendarCell = ({ day, cellCardList }: Props) => {
           {meetCard && (
             <div
               // className={`absolute top-2 w-[${(30 * meetCard[0].range).toString()}px] bg-slate-500`}>
-              className={`absolute top-2`}
+              className={`absolute top-2 z-50 cursor-pointer`}
               style={{ ...meetCardStyle }}
+              onMouseOver={() =>
+                handleHoverItem(`${meetCard.type}-${meetCard.typeId}`)
+              }
+              onMouseLeave={() => handleHoverItem("")}
+              onClick={() => {
+                router.push(`/meets/${meetCard.typeId}`);
+              }}
             >
-              <div className={`w-full ${cardBg}`}>{meetCard.content}</div>
+              {/* <div className={`w-full ${cardBg}`}>{meetCard.content}</div> */}
+              <div
+                className={`w-full ${hoverItem === `${meetCard.type}-${meetCard.typeId}` ? "bg-red-50" : "bg-slate-200"}`}
+              >
+                {meetCard.content}
+              </div>
             </div>
           )}
         </div>
--- src/_components/calendar/CalendarTable.tsx
+++ src/_components/calendar/CalendarTable.tsx
@@ -6,7 +6,7 @@ import {
   startOfMonth,
   startOfWeek
 } from "date-fns";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import CalendarCell from "./CalendarCell";
 import { CellCardTable } from "./type/schedule.types";
 
@@ -18,35 +18,26 @@ const CalendarTable = ({
   currentMonth: Date;
   cellCardTable: CellCardTable;
 }) => {
-  // 이번 달의 시작 일자(요일 등 포함)
+  const [hoverItem, setHoverItem] = useState("");
+
+  const handleHoverItem = (id: string) => {
+    console.log("id", id);
+    setHoverItem(id);
+  };
+
   const monthStart = startOfMonth(currentMonth);
-
-  // 이번 달의 시작 일자 기준 주의 시작
-  const dayStart = startOfWeek(monthStart);
-
-  // 이번 달의 마지막 일자
   const monthEnd = endOfMonth(currentMonth);
 
-  // 이번 달의 마지막 일자 기준 주의 마지막
+  const dayStart = startOfWeek(monthStart);
   const dayEnd = endOfWeek(monthEnd);
-
-  // dayStart ~ dayEnd 까지 7줄 씩 뿌려주면 됨
 
   const days: Date[] = useMemo(() => {
     const dayList = [];
     for (let i = dayStart; i <= dayEnd; i = addDays(i, 1)) {
       dayList.push(i);
     }
-    console.log("dayList", dayList);
     return dayList;
   }, [currentMonth]);
-
-  // const days = [];
-
-  // for (let i = dayStart; i <= dayEnd; i = addDays(i, 1)) {
-  //   days.push(i);
-  // }
-  // return convertScheduleDataToCellCardTable(scheduleList);
 
   return (
     <ul className="grid grid-cols-7">
@@ -55,6 +46,8 @@ const CalendarTable = ({
           key={day.toISOString()}
           day={day}
           cellCardList={cellCardTable[format(day, "yyyy-MM-dd")]}
+          hoverItem={hoverItem}
+          handleHoverItem={handleHoverItem}
         />
       ))}
     </ul>
