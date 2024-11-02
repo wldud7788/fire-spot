33d2b635bedb967f92cd16c9d38301c9b1916fee
김민규 <tjfvndfud@gmail.com>
2024-11-02 15:32:41 +0900

    update: 페이지네이션 클릭 시 상단 스크롤 이벤트 추가 및 CampCard에  props로 paramsId 값 전달 - useEffect 의존성 배열에 page를 추가하여 page 상태가 변경될 때 마다 스크롤 최상단으로 이동 - CampCard 컴포넌트에 있는 Link의 href값을 params로 적용하기 위해 props로 paramsId값을 전달해줌

--- src/_components/camp/CampList.tsx
+++ src/_components/camp/CampList.tsx
@@ -4,6 +4,7 @@ import CampCard from "@/_components/camp/CampCard";
 import { Camp } from "@/app/(pages)/camps/types/Camp";
 import Pagination from "../pagination/Pagination";
 import usePagination from "../pagination/hooks/pagination";
+import { useEffect } from "react";
 
 type CampListProps = {
   camps: Camp[];
@@ -15,12 +16,17 @@ const CampList = ({ camps, itemsPerPage, paramsId }: CampListProps) => {
   const { currentItems, page, totalPages, movePagePrev, movePageNext } =
     usePagination({ items: camps, itemsPerPage, paramsId });
 
+  // 페이지 번호가 변경되면 해당 페이지로 스크롤 이동
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지 이동 시 상단으로 스크롤
+  }, [page]);
+
   return (
     <div className="camp_list">
       <ul className="list_box flex flex-wrap gap-[30px]">
         {currentItems.map((camp) => (
           <li key={camp.contentId} className="w-[calc(25%-30px)]">
-            <CampCard camp={camp} />
+            <CampCard camp={camp} listParamsId={paramsId} />
           </li>
         ))}
       </ul>
