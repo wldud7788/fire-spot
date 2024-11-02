d59f5c8b1506a0e04b3a9cc145a89fcf09cb24c4
김민규 <tjfvndfud@gmail.com>
2024-11-02 15:49:21 +0900

    update: staleTime 추가 => 마운트 될 때 캐시된 데이터 사용

--- src/app/(pages)/camps/[campPageId]/page.tsx
+++ src/app/(pages)/camps/[campPageId]/page.tsx
@@ -3,7 +3,6 @@
 import CampList from "@/_components/camp/CampList";
 import { getTotalData } from "@/_utils/serverActions/campApi";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
 
 type CampListProps = {
   params: {
@@ -15,23 +14,15 @@ const CampListPage = ({ params }: CampListProps) => {
   const {
     data: camps,
     isLoading,
-    isError,
-    refetch
+    isError
   } = useQuery({
     queryKey: ["camps"],
     queryFn: async () => getTotalData(),
-    refetchOnMount: true // 마운트 시 재요청
+    staleTime: 1000 * 60 * 60 * 24
   });
 
-  console.log(params);
-
-  useEffect(() => {
-    refetch();
-  }, []);
-
   if (isLoading) return <div>데이터가 로딩중입니다.</div>;
-  if (isError) return <div>에러가 발생했습니다.</div>;
-  if (!camps || camps.length === 0) return <div>데이터가 없습니다.</div>;
+  if (isError || !camps) return <div>에러가 발생했습니다.</div>;
 
   return (
     <CampList camps={camps} itemsPerPage={8} paramsId={params.campPageId} />
