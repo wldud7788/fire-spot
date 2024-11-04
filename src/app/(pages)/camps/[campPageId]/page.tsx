3054ad77729e38a4dc7d9d8821dbbf30ec59b627
김민규 <tjfvndfud@gmail.com>
2024-11-05 08:08:18 +0900

    update: 클라이언트 컴포넌트에서 서버 컴포넌트로 변경 해당 컴포넌트 내에 있던 로직은 CampList 컴포넌트로 이동

--- src/app/(pages)/camps/[campPageId]/page.tsx
+++ src/app/(pages)/camps/[campPageId]/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import CampList from "@/_components/camp/CampList";
-import { useQuery } from "@tanstack/react-query";
 
 type CampListProps = {
   params: {
@@ -9,28 +6,8 @@ type CampListProps = {
   };
 };
 
-const fetchTotalData = async () => {
-  const response = await fetch("/api/campApi");
-  return response.json();
-};
-
-const CampListPage = ({ params }: CampListProps) => {
-  const {
-    data: camps,
-    isLoading,
-    isError
-  } = useQuery({
-    queryKey: ["camps"],
-    queryFn: async () => fetchTotalData(),
-    staleTime: 1000 * 60 * 60 * 24
-  });
-
-  if (isLoading) return <div>데이터가 로딩중입니다.</div>;
-  if (isError || !camps) return <div>에러가 발생했습니다.</div>;
-
-  return (
-    <CampList camps={camps} itemsPerPage={8} paramsId={params.campPageId} />
-  );
-};
+const CampListPage = ({ params }: CampListProps) => (
+  <CampList itemsPerPage={8} paramsId={params.campPageId} />
+);
 
 export default CampListPage;
