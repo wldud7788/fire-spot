0f1157a03290489a3ed12222e29e914358f847d3
Lee Gee Young <rooftop7788@naver.com>
2024-11-11 13:00:53 +0900

    feat: 모임 상세에 지도 추가

--- src/app/(pages)/meets/components/meetsDetail/MeetDetailController.tsx
+++ src/app/(pages)/meets/components/meetsDetail/MeetDetailController.tsx
@@ -6,13 +6,15 @@ import MeetIntroSection from "./MeetIntroSection";
 import MeetSuppliesSection from "./MeetSuppliesSection";
 import MeetRecommendSection from "./MeetRecommendSection";
 import useMeetDetailController from "../../hooks/useMeetDetailController";
+import DetailMap from "@/app/(pages)/camp-detail/components/DetailMap";
+import { Camp } from "@/app/(pages)/camps/types/Camp";
 
 type Props = {
   meetWithCamp: MeetWithCamp;
 };
 const MeetDetailController = ({ meetWithCamp }: Props) => {
   const { buttonConfig, hasChatAccess } = useMeetDetailController(meetWithCamp);
-
+  const camp = meetWithCamp.camp as unknown as Camp;
   return (
     <div className="mx-auto w-full max-w-[1360px] pl-[30px] pr-[30px]">
       <MeetTitleSection
@@ -23,6 +25,7 @@ const MeetDetailController = ({ meetWithCamp }: Props) => {
       <MeetIntroSection meetWithCamp={meetWithCamp} />
       <MeetContentSection meetWithCamp={meetWithCamp} />
       <MeetSuppliesSection meetWithCamp={meetWithCamp} />
+      <DetailMap camp={camp} />
       <MeetRecommendSection />
     </div>
   );
