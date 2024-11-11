073852e796a1bcd31641fd4baa6c2ffbc392b860
Lee Gee Young <rooftop7788@naver.com>
2024-11-12 08:37:41 +0900

    refactor: 태그 상수화

--- src/_components/main/MainSearch.tsx
+++ src/_components/main/MainSearch.tsx
@@ -1,27 +1,18 @@
 import React from "react";
 import SearchBar from "../search/searchBar/SearchBar";
 import Link from "next/link";
+import { TAGS } from "@/_utils/common/constant";
 
 const MainSearch = () => {
   return (
-    <div className="mt-10 flex flex-col items-center">
-      <ul className="flex gap-5">
-        <Link href={"./search?campingTypes=일반야영장"}>
-          <li>#캠핑</li>
-        </Link>
-        <Link href={"./search?campingTypes=자동차야영장"}>
-          <li>#차박</li>
-        </Link>
-        <Link href={"./search?campingTypes=글램핑"}>
-          <li>#글램핑</li>
-        </Link>
-        <Link href={"./search?amenities=내부화장실"}>
-          <li>#내부화장실</li>
-        </Link>
-        <Link href={"./search?petOption=가능"}>
-          <li>#애견동반 가능</li>
-        </Link>
-      </ul>
+    <div className="mt-10 flex flex-col items-center text-lg font-semibold">
+      <div className="flex gap-5">
+        {TAGS.map((tag) => (
+          <Link key={tag.name} href={tag.href} className="text-[#FF924C]">
+            {tag.name}
+          </Link>
+        ))}
+      </div>
       <div className="mt-[14px] w-full max-w-[800px] flex-1">
         <SearchBar variant="main" />
       </div>
--- src/_utils/common/constant.ts
+++ src/_utils/common/constant.ts
@@ -2,3 +2,15 @@ export const DEADLINE_APPROACHING = 3;
 
 export const MIN_HEADCOUNT = 3;
 export const MAX_HEADCOUNT = 20;
+
+export const MIN_SEARCH_LENGTH = 2; // 최소 검색어 길이
+//허용되지 않는 특수문자 정규식
+export const SPECIAL_CHARS = /[!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?]+/;
+
+export const TAGS = [
+  { name: "#캠핑", href: "/search?campingTypes=일반야영장" },
+  { name: "#차박", href: "/search?campingTypes=자동차야영장" },
+  { name: "#글램핑", href: "/search?campingTypes=글램핑" },
+  { name: "#내부화장실", href: "/search?amenities=내부화장실" },
+  { name: "#애견동반 가능", href: "/search?petOption=가능" }
+];
