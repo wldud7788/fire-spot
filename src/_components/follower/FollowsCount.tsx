39391068dfb6838f4e523c96b0fbe6713317dfab
김민규 <tjfvndfud@gmail.com>
2024-11-11 22:45:29 +0900

    update: 팔로워 컴포넌트 스타일 1차 완료

--- src/_components/follower/FollowsCount.tsx
+++ src/_components/follower/FollowsCount.tsx
@@ -7,8 +7,10 @@ type FollowsCountProps = {
 
 const FollowsCount = ({ followerCount, followingCount }: FollowsCountProps) => {
   return (
-    <ul className="flex">
-      <li>팔로워 {followerCount}</li>
+    <ul className="mb-[24px] flex items-center justify-center">
+      <li className="li-before-line relative mr-[13px] pr-[13px]">
+        팔로워 {followerCount}
+      </li>
       <li>팔로잉 {followingCount}</li>
     </ul>
   );
