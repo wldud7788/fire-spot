73fcf48f15578ab00cf1a0a897d300c17b3b3242
김민규 <tjfvndfud@gmail.com>
2024-11-13 02:20:57 +0900

    update: sos 리스트 1차 완료, 페이지네이션 적용 필요

--- src/app/(pages)/sos/page.tsx
+++ src/app/(pages)/sos/page.tsx
@@ -5,6 +5,8 @@ import { SosWithCamp } from "./types/sos.types";
 import { getSosList } from "./service/sosSerivce";
 import SosCard from "./components/sos/SosCard";
 import { Metadata } from "next";
+import CommunityTop from "@/_components/common/CommunityTop";
+import NoData from "@/_components/common/NoData";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -16,19 +18,37 @@ export async function generateMetadata(): Promise<Metadata> {
 const SosList = async () => {
   const sosWithCampList: SosWithCamp[] = await getSosList();
 
-  // TODO 민규님: SOS 목록 페이지
   return (
     <div className="relative mx-auto w-full max-w-[1360px] px-[30px]">
-      <div className="absolute right-[30px] top-4 h-[40px]">
-        <WriteButton url={SERVER_PAGE_URL.sosWrite}>작성으로 가자</WriteButton>
+      <CommunityTop text={"SOS 도움요청"} />
+      <div className="utils flex items-center justify-between">
+        <div className="flex items-center gap-[14px]">
+          <button className="rounded-[20px] border border-[#a6a6a6] px-[16px] py-[10px] text-[14px] font-bold">
+            진행중
+          </button>
+          <button className="rounded-[20px] border border-[#a6a6a6] px-[16px] py-[10px] text-[14px] font-bold">
+            종료됨
+          </button>
+        </div>
+        <div>
+          <WriteButton url={SERVER_PAGE_URL.sosWrite}>
+            도움 요청하기
+          </WriteButton>
+        </div>
       </div>
-      <ul className="mt-[40px] flex flex-col items-center gap-[23px]">
-        {sosWithCampList.map((sosWithCamp) => (
-          <li key={sosWithCamp.sos.id} className="w-full">
-            <SosCard sosWithCamp={sosWithCamp} />
-          </li>
-        ))}
-      </ul>
+
+      {sosWithCampList ? (
+        <ul className="mt-[40px] flex flex-col items-center gap-[23px]">
+          {sosWithCampList.map((sosWithCamp) => (
+            <li key={sosWithCamp.sos.id} className="w-full">
+              <SosCard sosWithCamp={sosWithCamp} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <NoData text={"등록된 모임이 없어요."} />
+      )}
+      <div className="my-[65px]">{"페이지네이션"}</div>
     </div>
   );
 };
