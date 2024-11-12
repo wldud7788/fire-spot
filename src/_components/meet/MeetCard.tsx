8973c7b919fa218210953132393de576ce0e6529
김민규 <tjfvndfud@gmail.com>
2024-11-12 14:28:54 +0900

    update: 현재 시안에 맞춰 ui 수정 및 스타일 처리

--- src/_components/meet/MeetCard.tsx
+++ src/_components/meet/MeetCard.tsx
@@ -8,37 +8,51 @@ type MeetCardProps = {
 
 const MeetCard = ({ meetCard }: MeetCardProps) => {
   return (
-    <div className="meet_card rounded-[20px] bg-[#F4F4F4] px-[30px] py-[25px]">
+    <div className="meet_card rounded-[20px] bg-[#ffefe5] px-[30px] py-[25px]">
       <div className="inner">
         <div className="utils flex items-center justify-between">
-          <ul className="flex gap-[10px]">
+          <ul className="flex w-full max-w-[calc(100%-40px)] flex-wrap gap-[10px]">
             {meetCard.tags.map((tag) => {
               return (
                 <li
                   key={tag}
-                  className="rounded-[15px] bg-[#D9D9D9] px-[15px] py-[3px] text-[15px]"
+                  className="rounded-[20px] border border-[#404040] bg-[#fff] px-[12px] py-[5px] text-[12px]"
                 >
                   {tag}
                 </li>
               );
             })}
           </ul>
-          <button type="button">북마크</button>
+          <div className="flex w-[40px] justify-end">
+            <button type="button">
+              <img
+                src="/assets/images/camp/ico-camp-list-bookmark.svg"
+                alt="북마크"
+              />
+              {/* 
+                이윤지 작업 - 북마크 액티브 되면 아래의 아이콘 쓰시면 됩니다. 
+                <img
+                  src="/assets/images/camp/ico-camp-list-bookmark-on.svg"
+                  alt="북마크"
+                /> 
+              */}
+            </button>
+          </div>
         </div>
         <Link
           href={`/meets/${meetCard.id}`}
-          className="mb-[10px] mt-[10px] block text-[30px] font-medium"
+          className="mb-[15px] mt-[15px] block text-[18px] font-bold"
         >
           {meetCard.title}
         </Link>
         <div className="info flex gap-[20px]">
-          <p className="bg-location bg-left-center-0 bg-no-repeat pl-[20px] text-[16px]">
+          <p className="color-gray01 bg-location bg-left-center-0 bg-no-repeat pl-[20px] text-[14px]">
             {meetCard.location}
           </p>
-          <p className="bg-date bg-left-center-0 bg-no-repeat pl-[18px] text-[16px]">
+          <p className="color-gray01 bg-date bg-left-center-0 bg-no-repeat pl-[18px] text-[14px]">
             {meetCard.date}
           </p>
-          <p className="bg-group bg-left-center-0 bg-no-repeat pl-[18px] text-[16px]">
+          <p className="color-gray01 bg-group bg-left-center-0 bg-no-repeat pl-[18px] text-[14px]">
             {meetCard.attendee_count}/{meetCard.deadline_headcount}
           </p>
         </div>
