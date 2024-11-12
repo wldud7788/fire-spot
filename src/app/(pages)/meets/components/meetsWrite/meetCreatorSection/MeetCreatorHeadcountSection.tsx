b143a29a81c9c630fb7a2e46b3b90228c7981695
김민규 <tjfvndfud@gmail.com>
2024-11-12 20:46:54 +0900

    update: 모임 작성 페이지 1차 스타일 완료

--- src/app/(pages)/meets/components/meetsWrite/meetCreatorSection/MeetCreatorHeadcountSection.tsx
+++ src/app/(pages)/meets/components/meetsWrite/meetCreatorSection/MeetCreatorHeadcountSection.tsx
@@ -33,54 +33,58 @@ const MeetCreatorHeadcountSection = ({ register, setValue, watch }: Props) => {
   };
 
   return (
-    <section className="mt-40 flex flex-col gap-5 rounded-[20px] border-[1px] border-[#B5B5B5] px-52 py-10">
-      <div className="flex items-center gap-3">
-        <h2 className="text-[20px]">참여 인원</h2>
-        <p className="text-[15px]">(모임장 포함)</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <input
-          className="h-[45px] w-[192px] rounded-[6px] border-2 bg-white px-4"
-          placeholder="최소 3명 이상 입력하세요"
-          type="number"
-          disabled
-          {...register("deadline_headcount", {
-            min: MIN_HEADCOUNT,
-            max: MAX_HEADCOUNT,
-            required: true
-          })}
-        />
-        <div className="flex gap-[9px]">
-          <button
-            type="button"
-            className="rounded-[11px] border-[1px] border-[#C3C3C3]"
-            onClick={handlePlus}
-          >
-            <div className="h-6 w-6 bg-plus bg-cover bg-center" />
-          </button>
-          <button
-            type="button"
-            className="rounded-[11px] border-[1px] border-[#C3C3C3]"
-            onClick={handleMinus}
-          >
-            <div className="h-6 w-6 bg-minus bg-cover bg-center" />
-          </button>
+    <section className="mt-[40px] rounded-[12px] border border-[#D9D9D9] px-[30px] py-[40px]">
+      <h2 className="flex items-center gap-[5px]">
+        <p className="color-gray01 text-[24px] font-bold">참여인원</p>
+        <span className="text-[rgba(178, 70, 0, 0.7)] text-[24px] font-bold">
+          (모임장 포함)
+        </span>
+      </h2>
+      <div className="mt-[40px] flex items-center gap-[60px]">
+        <div className="flex w-full max-w-[410px] items-center gap-[20px]">
+          <p className="color-gray01 text-[18px] font-medium">모집 인원</p>
+          <div className="flex items-center gap-[5px]">
+            <input
+              className="h-[47px] rounded-[11px] border-[1px] border-[#C3C3C3] bg-white px-[16px] py-[12px] text-right"
+              placeholder="최소 3명 이상 입력하세요"
+              type="number"
+              disabled
+              {...register("deadline_headcount", {
+                min: MIN_HEADCOUNT,
+                max: MAX_HEADCOUNT,
+                required: true
+              })}
+            />
+            <button
+              type="button"
+              className="flex h-[47px] w-[47px] items-center justify-center rounded-[11px] border-[1px] border-[#C3C3C3]"
+              onClick={handlePlus}
+            >
+              <div className="h-6 w-6 bg-plus bg-cover bg-center" />
+            </button>
+            <button
+              type="button"
+              className="flex h-[47px] w-[47px] items-center justify-center rounded-[11px] border-[1px] border-[#C3C3C3]"
+              onClick={handleMinus}
+            >
+              <div className="h-6 w-6 bg-minus bg-cover bg-center" />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="flex w-[479px] flex-col rounded-[6px] bg-[#EEE] px-[21px] py-[11px] text-sm text-[#A4A4A4]">
-        <div className="flex items-center gap-[3px]">
-          <div
-            className="h-[15px] w-[15px] bg-infoRound bg-cover bg-center"
-            style={{ fill: "#A4A4A4" }}
-          />
-          <p className="font-[500]">인원 설정 시 참고하세요</p>
-        </div>
-        <div>
-          <p>
-            ⸰ 실제 모임 진행의 필요한 최소 인원으로 설정해주세요.
-            <br />⸰ 마감시간 1시간 전까지 최소 인원(모임장 포함)이 모집되지
-            않으면 모임이 취소될 수 있어요.
+
+        <div className="import_box">
+          <p className="mb-[5px] bg-import bg-left-center-0 bg-no-repeat pl-[24px] text-[16px] text-[#a4a4a4]">
+            인원 설정 시 참고하세요
           </p>
+          <div className="text_box pl-[10px]">
+            <p className="before-dot-left relative pl-[10px] text-[14px] text-[#a4a4a4]">
+              실제 모임 진행의 필요한 최소 인원으로 설정해주세요.
+            </p>
+            <p className="before-dot-left relative pl-[10px] text-[14px] text-[#a4a4a4]">
+              마감시간 1시간 전까지 최소 인원(모임장 포함)이 모집되지 않으면
+              모임이 취소될 수 있어요.
+            </p>
+          </div>
         </div>
       </div>
     </section>
