d5ebfee92e145cc91911268c569ebd4873255c21
LeeJY97 <ijoonyeol97@gmail.com>
2024-11-13 20:29:21 +0900

    add: ChatRoomSection 파일 생성 (채팅방 하나로 합치기 위함)

---
+++ src/_components/chat/ChatRoomSection.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import ChatRoomMessageSection from "@/_components/chat/ChatRoomMessageSection";
+import ChatRoomTitleSection from "@/_components/chat/ChatRoomTitleSection";
+import { useChatSubscriptionMessageList } from "@/_components/chat/hooks/useChatSubscriptionMessageList";
+import {
+  useChatRoomMessage,
+  useChatRoomTitle
+} from "@/_components/chat/hooks/useChatRoom";
+import useUser from "@/_hooks/useUser";
+
+type Props = {
+  params: {
+    roomId: string;
+  };
+};
+
+const ChatRoomSection = ({ params }: Props) => {
+  // 미들웨어에서 접근 차단 및 roomId NaN 같은거 나와도 차단
+
+  const roomId = Number(params.roomId);
+  const user = useUser();
+  const { chatRoomTitle } = useChatRoomTitle(roomId);
+  const { messagesByDate } = useChatRoomMessage(roomId);
+
+  useChatSubscriptionMessageList(user?.id || "", roomId);
+  useChatSubscriptionMessageList(user?.id || "", roomId);
+
+  if (!user) return <></>;
+
+  return (
+    <div>
+      <ChatRoomTitleSection chatRoomTitle={chatRoomTitle} />
+      <ChatRoomMessageSection
+        loginUserId={user.id}
+        roomId={roomId}
+        messagesByDate={messagesByDate}
+      />
+    </div>
+  );
+};
+
+export default ChatRoomSection;
