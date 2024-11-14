6fdcc76261590a1d1c7b75fcb63606cd0f43ffaf
LeeJY97 <ijoonyeol97@gmail.com>
2024-11-14 21:09:12 +0900

    fix:[#70] - 메인 화면 프로필 정보 profile 테이블 기준으로 보여지도록 수정

--- src/_utils/auth.ts
+++ src/_utils/auth.ts
@@ -1,5 +1,10 @@
 import { User } from "@supabase/supabase-js";
 import { createClient } from "./supabase/client";
+import { getUserProfile } from "./service/followService";
+import {
+  ProfileSelect,
+  UserWithProfile
+} from "@/_components/chat/types/Profile.types";
 
 const supabase = createClient();
 
@@ -8,6 +13,23 @@ export const getUser = async (): Promise<User | null> => {
     data: { user }
   } = await supabase.auth.getUser();
   return user;
+};
+
+export const getUserWithProfile = async (): Promise<UserWithProfile | null> => {
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  const profile = await getUserProfile(user?.id || "");
+
+  const userWithProfile: UserWithProfile = {
+    user: user,
+    profile: profile
+  };
+
+  return userWithProfile;
+
+  // return user;
 };
 
 export const signOut = async (): Promise<void> => {
