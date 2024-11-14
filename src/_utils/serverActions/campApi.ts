9876529afe98c27eec11c2e6d56bf04357f42d39
Lee Gee Young <rooftop7788@naver.com>
2024-11-15 01:30:04 +0900

    fix: 사용하지 않는 서버액션 삭제

--- src/_utils/serverActions/campApi.ts
+++ src/_utils/serverActions/campApi.ts
@@ -14,13 +14,14 @@ import {
   CampImageList,
   CampSelect
 } from "@/app/(pages)/meets/types/camp.types";
+import { QUERYSTRING } from "../common/constant";
 
 export const getTotalData = async (
   page?: number | null,
   numOfRows?: number
 ) => {
   const res = await fetch(
-    `${GOCAMPING_HOST}${GOCAMPING_ALL}?serviceKey=${GOCAMPING_KEY}&numOfRows=${numOfRows ? numOfRows : 4044}&pageNo=${page ? page : "max"}&MobileOS=ETC&MobileApp=TestApp&_type=json`,
+    `${GOCAMPING_HOST}${GOCAMPING_ALL}?serviceKey=${GOCAMPING_KEY}&numOfRows=${numOfRows ? numOfRows : 4044}&pageNo=${page ? page : "max"}&${QUERYSTRING}`,
     {
       next: {
         revalidate: 86400
@@ -79,26 +80,5 @@ export const getCampDataFromDB = async (
   } catch (error) {
     console.error(error);
     throw new Error("Error getCampDataFromDB ");
-  }
-};
-// TODO - 준열,지영 각자 쓰던 함수 통일 필요, numOfRows=100 설정 체크 필요
-
-export const getSearchCampsData = async (keyword: string) => {
-  try {
-    const res = await fetch(
-      `${GOCAMPING_HOST}${GOCAMPING_SEARCH}?serviceKey=${GOCAMPING_KEY}&MobileOS=ETC&MobileApp=AppTest&_type=json&keyword=${encodeURIComponent(keyword)}&pageNo=1&numOfRows=100`,
-      {
-        next: {
-          revalidate: 86400
-        }
-      }
-    );
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const data = await res.json();
-    return data.response.body.items.item;
-  } catch (error) {
-    console.error("Error fetching data:", error);
   }
 };
