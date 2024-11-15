304720f6501211570e230ac30bcf4f5c32dcec4d
김민규 <tjfvndfud@gmail.com>
2024-11-15 15:51:02 +0900

    update: 좋아요한 캠핑장이 없어요 추가

--- src/app/(pages)/mypage/Likes.tsx
+++ src/app/(pages)/mypage/Likes.tsx
@@ -10,7 +10,11 @@ const Likes: React.FC = () => {
         <h2 className="mb-[40px] text-[18px] font-bold">
           내가 좋아요한 캠핑장
         </h2>
-        <LikeList />
+        {LikeList.length ? (
+          <LikeList />
+        ) : (
+          <NoData text={"좋아요한 캠핑이 없어요."} />
+        )}
       </div>
       <div>
         <h2 className="mb-[40px] text-[18px] font-bold">내가 좋아요한 모임</h2>
