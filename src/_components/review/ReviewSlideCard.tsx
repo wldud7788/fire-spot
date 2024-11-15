e2535a347d626005ca5c8cc3c34d9f6391cd1fc6
김민규 <tjfvndfud@gmail.com>
2024-11-15 12:22:14 +0900

    update: 캠프 상세, 마이페이지 리뷰, 모달 리뷰 스타일 완료

--- src/_components/review/ReviewSlideCard.tsx
+++ src/_components/review/ReviewSlideCard.tsx
@@ -15,14 +15,39 @@ const ReviewSlideCard: React.FC<ReviewSlideCardProps> = ({
 }) => {
   // 이윤지: 이미지 없는 경우 어떻게 할 지 생각하기
   const imgUrl = review.img.length > 0 ? review.img[0] : "";
-
+  console.log("review ====>", review);
   return (
-    <div onClick={onClickFunc}>
-      <h2>{review.campId}</h2>
-      <h3>{review.title}</h3>
-      <p>{review.content}</p>
-      <p>{review.likes}</p>
-      <img src={imgUrl} alt="" />
+    <div
+      onClick={onClickFunc}
+      className="flex min-h-[245px] flex-col justify-between overflow-hidden rounded-[12px] border border-[#BFBFBF] px-[24px] py-[28px]"
+    >
+      {/* <h2>{review.campId}</h2> 어디에 사용하는지 모르겠습니다.*/}
+      <div>
+        <div className="flex items-center">
+          <h3 className="text-[16px] font-bold">{review.title}</h3>
+        </div>
+        <div className="mb-[16px] mt-[12px]">
+          <p className="line-clamp-3 text-[14px]">{review.content}</p>
+        </div>
+      </div>
+      <div className="flex items-end justify-between">
+        <div className="flex items-center gap-[5px]">
+          <p className="color-gray04 text-[14px]">
+            좋아요: {review.likes ? review.likes : 0}개
+          </p>
+          <p className="color-gray04 before-dot-left relative pl-[10px] text-[14px]">
+            {review.date}
+          </p>
+        </div>
+
+        <div className="h-[80px] w-[90px] overflow-hidden rounded-[6px] shadow-custom">
+          {imgUrl ? (
+            <img src={imgUrl} alt="" className="h-full w-full object-cover" />
+          ) : (
+            <div className="w-full"></div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
