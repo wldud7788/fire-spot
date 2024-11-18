81ba5de6090d4d8793b28d96d7f1d102770f2170
Lee Gee Young <rooftop7788@naver.com>
2024-11-18 16:49:42 +0900

    fix: 짜잘이 오류 수정

--- src/app/(pages)/search/components/CampCard.tsx
+++ src/app/(pages)/search/components/CampCard.tsx
@@ -27,12 +27,14 @@ export const CampCard: React.FC<CampCardProps> = ({ camp, onSelect }) => {
         onClick={() => onSelect(camp)}
         className="flex w-full flex-row items-start space-x-4"
       >
-        <div className="relative h-[100px] w-[100px] flex-shrink-0 overflow-hidden rounded-lg">
+        <div className="relative h-[110px] w-[110px] flex-shrink-0 overflow-hidden rounded-lg text-left">
           <Image
-            src={camp.firstImageUrl || "/assets/images/default_profile.jpeg"}
+            src={
+              camp.firstImageUrl || "/assets/images/camp/img-camp-default.jpg"
+            }
             className="object-cover transition-all duration-500 ease-in-out group-hover:scale-110"
             fill
-            sizes="100px"
+            sizes="200px"
             alt={camp.facltNm}
           />
           <span className="absolute right-2 top-2 max-w-[80px] truncate rounded bg-white/90 px-2 py-1 text-xs font-medium text-gray-700">
@@ -40,7 +42,7 @@ export const CampCard: React.FC<CampCardProps> = ({ camp, onSelect }) => {
           </span>
         </div>
 
-        <div className="flex min-w-0 flex-1 flex-col space-y-2">
+        <div className="flex min-w-0 flex-1 flex-col space-y-2 text-left">
           <h2 className="truncate text-lg font-bold text-gray-900">
             {camp.facltNm}
           </h2>
@@ -58,7 +60,7 @@ export const CampCard: React.FC<CampCardProps> = ({ camp, onSelect }) => {
 
           <div className="min-w-0 space-y-1 text-sm text-gray-600">
             <p className="truncate">{camp.addr1}</p>
-            <div className="flex min-w-0 items-center justify-center space-x-2 text-xs">
+            <div className="flex min-w-0 items-center space-x-2 text-xs">
               {/* <span className="truncate font-medium text-gray-900">249Km</span> */}
               {/* <span className="h-1 w-1 flex-shrink-0 rounded-full bg-gray-300"></span> */}
               <span className="truncate">{camp.induty}</span>
