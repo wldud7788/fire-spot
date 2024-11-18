b04084faee54b5ec31cd6c92c6412fc29ab86c65
Lee Gee Young <rooftop7788@naver.com>
2024-11-18 13:42:41 +0900

    style: header 스타일 수정, footer 스타일 수정

--- src/_components/layout/Footer.tsx
+++ src/_components/layout/Footer.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-screen leading-10">
-      <ul className="flex h-[80px] items-center justify-center gap-5 bg-[#DCDCDC] text-sm text-[#707070]">
+    <footer className="w-full">
+      <ul className="flex flex-col items-center justify-center gap-2 bg-gray-200 py-3 text-sm text-gray-500 md:flex-row md:gap-5 md:py-4">
+        <div className="flex items-center gap-3">
+          <li>
+            <Link href="/">이용약관</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href="/">개인정보처리방침</Link>
+          </li>
+        </div>
+        <span className="hidden md:block">|</span>
         <li>
-          <Link href={"/"}>이용약관 </Link>
-        </li>
-        <span>|</span>
-        <li>
-          <Link href={"/"}>개인정보처리방침 </Link>
-        </li>
-        <span>|</span>
-        <li>
-          <Link href={"/"}>Copyright ⓒ 불멍스팟 All rights reserved</Link>
+          <Link href="/">Copyright ⓒ 불멍스팟 All rights reserved</Link>
         </li>
       </ul>
     </footer>
